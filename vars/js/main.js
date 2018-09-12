bedaad59768a611bf9cd9cd4e5cd75241f2a45cb
Jocel Markus Quitoriano <42975022+jquitoriano1@users.noreply.github.com>
2018-09-12 08:59:26 -0600

    javascript finally

---
+++ vars/js/main.js
@@ -0,0 +1,54 @@
+// console.log();
+// dot syntax
+/* 
+   JS
+   Objects
+     - properties
+     - methods 
+     - events
+
+     Style EMCAScript
+     - standard rules spec
+     - ES5 spec
+     - ES6
+     - ES2017
+     - ES2018
+
+    Rules ECMA
+    = dot syntax
+    statement JS
+    obj.(prop/method).value
+
+    Property Statement
+    object.prop = value
+    navigator.name   //read only
+    window.name="new window";  Read/Write  or Read Only
+
+    Method Statement
+    object.method('args');
+    console.log('help me');
+
+    Variables JS
+    var name assignment operator  value
+    creating variable names BP
+      no numbers, no uppercase first Letter, = ? @ 
+      no reserver charachter t ot the language.
+
+      BP Var Naming
+      be semantic about the content or use of the variable
+       var albertaTaxRate = 0.25;
+       albertTaxRate camel case;
+
+       data types of variables
+       string text ''
+       123 number
+       true and false   booleans
+       array, function, dom elemements.
+
+      
+*/
+// Decalaring a Variable
+
+var memberLoginForm = document.getElementById('member-signin')
+
+console.log(memberLoginForm)
---
+++ webflow/js/main.js
@@ -0,0 +1,28 @@
+/*
+    RULES ECMA
+    = dot syntax
+    statement JS
+    obj.(prop/method).value
+
+    Property Stament
+    object.prop= value
+    navigator.name //read only
+    window.name="new windows"; READ/WRITE or READ ONLY
+
+
+    Method Statement
+    object.method('args');
+    console.log('help me');
+
+    Variables JS
+    var name assignment operator value
+    creating variable names best practices
+        no numbers, no uppercase first letter, = ? @
+        no reserver character t ot the language
+
+        BP Var Naming
+        be semantic about the content or use of the variable
+        sample:     var albertaTaxRate = 0.25;
+*/
+
+console.log('Finally some js');
