23c323104241c754e8d9903a65ce38ead0d9773e
Jocel Markus Quitoriano <42975022+jquitoriano1@users.noreply.github.com>
2018-09-19 09:48:26 -0600

    more event listener

--- vars/js/main.js
+++ vars/js/main.js
@@ -49,6 +49,82 @@
 */
 // Decalaring a Variable
 
-var secondIcon = document.querySelector('.icon-view li:last-child')
+var secondIcon = document.querySelector('.icon-view li:last-child');
 
-console.log(secondIcon)
+console.log(secondIcon);
+
+//create a function that calculate the tax
+//funcions
+
+//hard coded variable
+var albertaTaxRate = 0.05;
+
+//parameters
+function calculateTax(price){
+  // perform the action
+  return albertaTaxRate * price;
+}
+
+// function call
+//sending arguments
+console.log('value returned:', calculateTax(100));
+
+//shipping calculator
+
+var perKilo = 2.30;
+
+function shippingCalculator(weight){
+  var total= perKilo * weight;
+  return '$' + total.toFixed(2);
+}
+
+console.log('The shipping rate is ', shippingCalculator(100));
+
+// another example
+/* create function send it a paragraph from the dom and change the text color and both color and dom element */
+// function paragraphColor(item,pColor){
+
+// }
+var shoppingCart = document.querySelector('#cart');
+
+// shoppingCart.addEventListener('click', onCheckOut);
+
+// function onCheckOut(evt){
+//   console.log(evt.target);
+// }
+
+
+
+
+
+
+
+//
+var loginButton = document.querySelector('#cart button');
+loginButton.addEventListener('click', onSubmitLogin);
+function onSubmitLogin(e){
+  console.log("lakad matatag!! normalin, normalin")
+}
+
+//system event
+window.addEventListener('load', function(e){
+  console.log(e)
+})
+//system event (start of every script) put everything inside
+window.addEventListener('DOMContentLoaded', function(e){
+  var formatOutput = document.querySelector('#cart button');
+  var taxcalculator = document.querySelector('#cart');
+  formatOutput.addEventListener('click', onFormatString);
+
+  function onFormatString(e){
+    var price = taxcalculator.querySelector('input:first-of-type').value;
+    var tax = taxcalculator.querySelector('input:last-of-type').value;
+
+    var totalPrice = (parseInt(price) * parseFloat(tax)) + parseInt(price);
+    taxcalculator.querySelector('textarea').value = "Total Price $: " + totalPrice.toFixed(2);
+    console.log(totalPrice);
+    
+  }
+})
+
+
