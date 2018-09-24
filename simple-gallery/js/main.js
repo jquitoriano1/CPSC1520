0e06a13a3f5adbf339985567dcef411740ce5226
Jocel Markus Quitoriano <42975022+jquitoriano1@users.noreply.github.com>
2018-09-24 09:46:03 -0600

    more button events

--- simple-gallery/js/main.js
+++ simple-gallery/js/main.js
@@ -1,17 +1,53 @@
 
 window.addEventListener('DOMContentLoaded',function(){
     // ALL our code is global to the function
-    var gallery = document.querySelector('.gallery');
-    //how to add and remove class from the attached css file
-    //elm.classList.add
-    //elm.classList.remove
-    //elm.classList.toogle
-    gallery.classList.add('red');
+    // var windowViewState = false;
+    // var gallery = document.querySelector('.gallery-display');
+    // var toogleButton = document.querySelector('.toggleBtn');
+    // //how to add and remove class from the attached css file
+    // //elm.classList.add
+    // //elm.classList.remove
+    // //elm.classList.toogle
+    // gallery.classList.add('red');
 
-    var toogleButton = gallery.querySelector('.toggleBtn');
-    toogleButton.addEventListener('click', hideImage);
-    function hideImage(e){
-        gallery.querySelector('.gallery-display').classList.toggle('hidden');
+    // toogleButton.addEventListener('click', hideImage);
+    // function hideImage(e){
+    //     // gallery.querySelector('.gallery-display').classList.toggle('hidden');
+    //     if( windowViewState === true){
+    //         windowViewState = false;
+    //         console.log("hide window");
+    //         gallery.classList.add('hidden');
+    //     }
+    //     else{
+    //         window.windowViewState = true;
+    //         gallery.classList.remove('hidden');
+    //     }
+    // }
+
+    var incrementBtn = document.querySelector('.counter button:first-of-type');
+    var decrementBtn = document.querySelector('.counter button:last-of-type');
+    var display = document.querySelector('.display');
+    var count = 5;
+
+    //setup user interface function...
+    function configUI(){
+        display.innerHTML = count;
     }
-        
-})
+
+    incrementBtn.addEventListener('click', onUpdateDisplay);
+    decrementBtn.addEventListener('click', onUpdateDisplay);
+
+    function onUpdateDisplay(e){
+        if(e.target.id === "up"){
+            count++;
+            console.log(count);
+        }
+        else{
+            count--
+            console.log(count);
+        }
+    }
+
+
+    configUI();
+});
